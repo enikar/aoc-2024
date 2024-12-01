fa588218b236da1d6c6f44c1c8bad568ea509ed7
enikar <enikar@gresille.org>
2024-12-01 20:49:32 +0100

    Add a new solution in haskell for the day1 using list.

---
+++ hday1-list.hs
@@ -0,0 +1,48 @@
+-- AoC 2024, Day 1
+
+module Main where
+
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
+import Data.List (sort, foldl', elemIndices)
+
+readPair :: String -> (Int, Int)
+readPair s = (a, b)
+  where
+    errRead = error "Error: readPair: not an Int in day1.txt"
+    readInt = fromMaybe errRead . readMaybe
+
+    (left, right) = case words s of
+      [le, ri] -> (le, ri)
+      _        -> error "Error: readPair: can't read a pair of Int"
+
+    a = readInt left
+    b = readInt right
+
+
+readDatas :: IO ([Int], [Int])
+readDatas = do
+  pairs <- map readPair . lines <$> readFile "day1.txt"
+  let (left, right) = unzip pairs
+  pure (sort left, sort right)
+
+
+main :: IO ()
+main = do
+  (left, right) <- readDatas
+  showSolution "Part1" (part1 left right)
+  showSolution "Part2" (part2 left right)
+
+showSolution :: String -> Int -> IO ()
+showSolution part sol =
+  putStrLn (part <> ": " <> show sol)
+
+part1 :: [Int] -> [Int] -> Int
+part1 vl vr = sum (zipWith f vl vr)
+  where
+    f x y = abs (x - y)
+
+part2 :: [Int] -> [Int] -> Int
+part2 vl vr = foldl' f 0 vl
+  where
+    f acc x = acc + x * length (elemIndices x vr)
