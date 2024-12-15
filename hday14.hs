55837f3472b6a99ef9918ff7e526626abaffd4f0
enikar <enikar@gresille.org>
2024-12-15 11:04:42 +0100

    Add a solution for day 14, part1
    Well, my solution is very laborious!!.

---
+++ hday14.hs
@@ -0,0 +1,174 @@
+-- AoC 2024 day 14, part1
+
+{-# LANGUAGE ImportQualifiedPost #-}
+
+{- HLINT ignore "Eta reduce" -}
+module Main (main) where
+
+import Data.Map.Strict qualified as M
+import Data.Map.Strict (Map, (!))
+import Data.List (foldl')
+
+-- module for parsing
+import Data.Char (isDigit)
+import Control.Monad (void)
+import Text.ParserCombinators.ReadP
+  (ReadP
+  ,readP_to_S
+  ,char
+  ,string
+  ,sepBy1
+  ,munch1
+  ,skipMany1
+  ,optional
+  ,option
+  ,eof
+  )
+
+type Robot = (Int, Int)    -- a pair of velocities (vx, vy)
+type Position = (Int, Int) -- columns and rows
+type Grid = Map Position [Robot]
+
+columns, rows :: Int
+columns = 101
+rows = 103
+xhalf, yhalf :: Int
+xhalf = columns `quot` 2
+yhalf = rows `quot` 2
+
+initialGrid :: Grid
+initialGrid = M.fromList grid0
+  where
+    grid0 = [((x, y), [])
+            | x <- [0..columns-1]
+            , y <- [0..rows-1]
+            ]
+
+quadrantLT, quadrantLD, quadrantRT, quadrantRD :: [Position]
+quadrantLT = [(x, y)
+              |x <- [0 .. xhalf-1]
+              , y <- [0 .. yhalf-1]
+              ]
+quadrantLD = [(x,y)
+              |x <- [0 .. xhalf-1]
+              ,y <- [yhalf+1 .. rows-1]
+              ]
+quadrantRT = [(x,y)
+              |x <- [xhalf+1 .. columns-1]
+              ,y <- [0 .. yhalf-1]
+              ]
+quadrantRD = [(x, y)
+              |x <- [xhalf+1 .. columns-1]
+              ,y <- [yhalf+1 .. rows-1]
+              ]
+
+
+getDatas :: String -> IO Grid
+getDatas filename = parseDatas <$> readFile filename
+
+printSolution :: Show a => String -> a -> IO ()
+printSolution part x = putStrLn (part <> ": " <> show x)
+
+main :: IO ()
+main = do
+  grid <- getDatas "day14.txt"
+  printSolution "Part1" (part1 grid)
+  --printSolution "Part2" (part2 grid)
+
+part1 :: Grid -> Int
+part1 grid = product
+             (map (countRobots grid')
+                   [quadrantLT
+                   ,quadrantLD
+                   ,quadrantRT
+                   ,quadrantRD
+                   ])
+
+  where
+    grid' = times 100 oneSecond grid
+
+-- TODO: part2
+-- part2 :: Grid -> Int
+-- part2 = undefined
+
+oneSecond :: Grid -> Grid
+oneSecond = M.foldlWithKey' f initialGrid
+  where
+    f grid (px, py) robots = foldl' g grid robots
+      where
+        g acc r@(vx, vy) = M.alter (updatePosition r) p acc
+          where
+            p = (move columns px vx, move rows py vy)
+
+move :: Int -> Int -> Int -> Int
+move modulo pos vel
+  | pos' < 0  = pos' + modulo
+  | pos' < modulo = pos'
+  | otherwise = pos' - modulo
+    where pos' = pos + vel
+
+-- borrow from: https://github.com/glguy/advent/blob/main/common/src/Advent/Prelude.hs
+times :: Int -> (a -> a) -> a -> a
+times n f x
+  | n <= 0    = x
+  | otherwise = times (n-1) f $! f x
+
+countRobots :: Grid -> [Position] -> Int
+countRobots grid coords = foldl' f 0 coords
+  where
+    f acc (x, y) = acc + length (grid ! (x, y))
+
+updatePosition :: Robot -> Maybe [Robot] -> Maybe [Robot]
+updatePosition robot Nothing = Just [robot]
+updatePosition robot (Just robots) = Just (robot:robots)
+
+-- parsing stuff
+parse :: ReadP a -> ReadS a
+parse = readP_to_S
+
+number :: ReadP Int
+number = do
+  optional (char '+')
+  signed <- option '+' (char '-')
+  n <- read <$> munch1 isDigit
+  let n' | signed == '+' = n
+         | otherwise     = -n
+  pure n'
+
+newLine :: ReadP ()
+newLine = void (char '\n')
+
+spaces :: ReadP ()
+spaces = skipMany1 (char ' ')
+
+parseDatas :: String -> Grid
+parseDatas str =
+  case parse readDatas str of
+    [x] -> fst x
+    []  -> error "Error: parseDatas: can't parse."
+    _   -> error "Error: parseDatas: there are more than one result."
+
+readDatas :: ReadP Grid
+readDatas = do
+  robots <- sepBy1 readRobot newLine
+  optional newLine
+  eof
+  pure (createGrid robots)
+
+readRobot :: ReadP (Position, Robot)
+readRobot = do
+  void (string "p=")
+  px <- number
+  void (char ',')
+  py <- number
+  spaces
+  void (string "v=")
+  vx <- number
+  void (char ',')
+  vy <- number
+  pure ((px, py), (vx, vy))
+
+createGrid :: [(Position, Robot)] -> Grid
+createGrid = foldl' f initialGrid
+  where
+    f acc (p, r) = M.alter (updatePosition r) p acc
